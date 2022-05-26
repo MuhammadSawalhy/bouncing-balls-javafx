acda6c9270e142a054e3ecc11f27e63db61a0d5f
محمد الصوالحي <ms.2052001@gmail.com>
2022-05-26 16:01:48 +0200

    Implement code for Vector class

---
+++ src/main/java/zagazig/cse/bouncingballs/balls/Vector.java
@@ -0,0 +1,45 @@
+package zagazig.cse.bouncingballs.balls;
+
+public class Vector {
+    double x;
+    double y;
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getMagnitude() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public void setMagnitude(double mag) {
+        double ang = this.getAngle();
+        this.x = mag * Math.cos(ang);
+        this.y = mag * Math.sin(ang);
+    }
+
+    public double getAngle() {
+        return Math.atan2(y, x);
+    }
+
+    public double distanceTo(Vector v) {
+        double dx = this.x - v.x;
+        double dy = this.y - v.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public void add(Vector v) {
+        this.x += v.x;
+        this.y += v.y;
+    }
+
+    public void subtract(Vector v) {
+        this.x -= v.x;
+        this.y -= v.y;
+    }
+
+    public void scale(double factor) {
+        setMagnitude(this.getMagnitude() * factor);
+    }
+}
